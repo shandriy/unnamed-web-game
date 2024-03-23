40bfb2263b826218bf1e68426c4305a3221e387f
entyspite <entyspite@gmail.com>
2024-03-23 13:29:28 -0700

    Replaced let with const
    for constants that is

--- main.js
+++ main.js
@@ -9,25 +9,42 @@ Konstantin Edunov, 2024
 */
 
 window.addEventListener("load", () => {
+  const nullURI = "data:application/octet-stream;base64,";
   let itemsLoaded = 1;
   let gearLoaded = false;
-  let gearIcon = new Image();
-  gearIcon.addEventListener("load", () => {
+  let gearIcon = [new Image()];
+  gearIcon[0].addEventListener("load", () => {
     gearLoaded = true;
     itemsLoaded++;
   });
-  gearIcon.src = "assets/gear.png";
+  let assetImportArray = ["assets/gear.png"]
+  function loadImagesWithArray(variableReferenceArray) {
+    let URLURI = nullURI;
+    let length = variableReferenceArray.length;
+    for (let cycle = 0; cycle < length; cycle++) {
+      URLURI = variableReferenceArray[cycle];
+      variableReferenceArray[cycle] = new Image();
+      gearIcon[0].addEventListener("load", () => {
+        gearLoaded = true;
+        itemsLoaded++;
+      });
+      gearIcon[0].src = "assets/gear.png";
+    };
+    variableReferenceArray[0] = "osu!";
+  };
+  loadImagesWithArray(assetImportArray);
+  gearIcon[0].src = "assets/gear.png";
   let deltaTime = 13, previousFrame = 0;
   let aspectRatio = 16 / 9, width = 400, height = 300;
-  let scaleWidth = 1600;
-  let scaleHeight = 900;
-  let expectedAspectRatio = 16 / 9;
-  let foreground = document.getElementById("foreground");
-  let stage = document.getElementById("stage");
-  let background = document.getElementById("background");
-  let foregroundContext = foreground.getContext("2d", { alpha: true });
-  let stageContext = stage.getContext("2d", { alpha: true });
-  let backgroundContext = background.getContext("2d", { alpha: true });
+  const scaleWidth = 1600;
+  const scaleHeight = 900;
+  const expectedAspectRatio = 16 / 9;
+  const foreground = document.getElementById("foreground");
+  const stage = document.getElementById("stage");
+  const background = document.getElementById("background");
+  const foregroundContext = foreground.getContext("2d", { alpha: true });
+  const stageContext = stage.getContext("2d", { alpha: true });
+  const backgroundContext = background.getContext("2d", { alpha: true });
   function resizeCanvases() {
     aspectRatio = window.innerWidth / window.innerHeight;
     if (aspectRatio < expectedAspectRatio) {
@@ -61,19 +78,19 @@ window.addEventListener("load", () => {
   window.addEventListener("resize", resizeCanvases);
   window.addEventListener("focus", resizeCanvases);
   function drawImage(context, image, x, y, scale) {
-    let scaleX = width / scaleWidth;
-    let scaleY = height / scaleHeight;
-    let imageWidth = image.naturalWidth * scale * scaleX;
-    let imageHeight = image.naturalHeight * scale * scaleY;
+    const scaleX = width / scaleWidth;
+    const scaleY = height / scaleHeight;
+    const imageWidth = image.naturalWidth * scale * scaleX;
+    const imageHeight = image.naturalHeight * scale * scaleY;
     context.drawImage(image, x * scaleX, y * scaleY, imageWidth, imageHeight);
   };
   function drawRotatedImage(context, image, x, y, scale, degrees) {
-    let scaleX = width / scaleWidth;
-    let scaleY = height / scaleHeight;
-    let imageWidth = image.naturalWidth * scale * scaleX;
-    let imageHeight = image.naturalHeight * scale * scaleY;
-    let positionX = Math.round(imageWidth / 2 * (-1));
-    let positionY = Math.round(imageHeight / 2 * (-1));
+    const scaleX = width / scaleWidth;
+    const scaleY = height / scaleHeight;
+    const imageWidth = image.naturalWidth * scale * scaleX;
+    const imageHeight = image.naturalHeight * scale * scaleY;
+    const positionX = Math.round(imageWidth / 2 * (-1));
+    const positionY = Math.round(imageHeight / 2 * (-1));
     context.save();
     context.translate(Math.round(x * scaleX + imageWidth / 2), Math.round(y * scaleY + imageHeight / 2));
     context.rotate((degrees % 360) / 57.2958);
@@ -86,7 +103,7 @@ window.addEventListener("load", () => {
     foregroundContext.clearRect(0, 0, width, height);
     stageContext.clearRect(0, 0, width, height);
     if (gearLoaded && itemsLoaded > 0) {
-      drawRotatedImage(foregroundContext, gearIcon, 736, 386, 2, thisFrame / itemsLoaded)
+      drawRotatedImage(foregroundContext, gearIcon[0], 736, 386, 2, thisFrame / itemsLoaded)
     };
     window.requestAnimationFrame(frame);
   };
