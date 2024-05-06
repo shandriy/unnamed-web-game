47936a52d0950c77b49e10d37f3f53139f96612e
shandriy <entyspite@gmail.com>
2024-05-05 17:59:08 -0700

    Added GNU GPLv3 license and notice

--- main.js
+++ main.js
@@ -1,5 +1,65 @@
 "use strict";
 
+/*
+---------==================================---------
+Copyright 2024 Konstantin Edunov
+
+THIS NOTICE MAY NOT BE REMOVED FROM ANY SOURCE
+REDISTRIBUTIONS OF THIS PROGRAM. IT MAY NOT BE
+MODIFIED OR CHANGED IN ANY WAY BY ANY OTHERS BESIDES
+THE ORIGINAL AUTHORS.
+
+This program is licensed under the GNU V3 general
+public license. This means that it is completely
+free and you may redistribute it as you wish as long
+as the following terms are met:
+- Redistributions must be under the same license.
+(GNU GPLv3)
+- The source must be stated.
+- All changes must be stated.
+- A copyright notice must be included.
+
+THIS LIST IS NOT CONCLUSIVE; ANYTHING MENTIONED IN
+THIS LIST IS ONLY TO GIVE THE READER A BRIEF IDEA OF
+THE LICENSING. FOR DETAILS, THE LICENSE CAN BE FOUND
+IN THE COPYING FILE IN THE ROOT DIRECTORY, AS WELL
+AS AT THE FOLLOWING URL:
+www.gnu.org/licenses/gpl-3.0-standalone.html
+
+If this is a source redistribution, it should
+include the GNU GPLv3 license. If you can not find
+it or it is missing, you can find it at the above
+listed URL. Alternatively, see the URL
+www.gnu.org/licenses for more information and
+detail.
+
+THIS PROGRAM DOES NOT COME WITH WARRANTY OF ANY
+KIND. KONSTANTIN EDUNOV, OR ANY OTHER AUTHORS OR
+CONTRIBUTORS ARE NOT RESPONSIBLE FOR MISUSE OF
+THE PROGRAM OR IF IT DOES WORK AS EXPECTED.
+
+AS STATED ABOUT THE LIST, NOTHING IN THIS NOTICE
+IS CONCLUSIVE. ANY CONTRADICTIONS BETWEEN THE GNU
+GPLv3 LICENSE AND THIS NOTICE ARE PURELY ACCIDENTAL
+AND THE AUTHORS OF THIS PROGRAM MAY NOT BE HELD
+LIABLE FOR MISUSE DUE TO THIS. IF YOU ARE UNSURE,
+THE GNU GPLv3 LICENSE WILL ALWAYS BE PRIORITIZED IN
+ANY CONTRADICTIONS.
+
+https://www.gnu.org/licenses/gpl-3.0-standalone
+https://www.gnu.org/licenses/gpl-3.0-standalone.html
+https://www.gnu.org/licenses
+www.gnu.org/licenses/gpl-3.0-standalone
+www.gnu.org/licenses/gpl-3.0-standalone.html
+www.gnu.org/licenses
+/COPYING
+
+A local version should be provided here: ./COPYING
+This is assuming neither this file nor the license
+have been moved.
+---------==================================---------
+*/
+
 var statusChecker = [];
 function statusCheckerMake(name, value) {
   var index = statusChecker.indexOf(name);
